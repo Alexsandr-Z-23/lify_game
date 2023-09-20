2c15b10fbf95c575b7d7034122ddc594f000d397
Alexsandr <alex.zelik.m@bk.ru>
2023-09-20 20:22:18 +0300

    Добавьте файлы проекта.

---
+++ lify_game/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace lify_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numPlot = new System.Windows.Forms.NumericUpDown();
+            this.numRezolu = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BTStop = new System.Windows.Forms.Button();
+            this.BTStart = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPlot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numRezolu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.numPlot);
+            this.splitContainer1.Panel1.Controls.Add(this.numRezolu);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.BTStop);
+            this.splitContainer1.Panel1.Controls.Add(this.BTStart);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 179;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Плотность";
+            // 
+            // numPlot
+            // 
+            this.numPlot.Location = new System.Drawing.Point(12, 189);
+            this.numPlot.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.numPlot.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numPlot.Name = "numPlot";
+            this.numPlot.Size = new System.Drawing.Size(120, 20);
+            this.numPlot.TabIndex = 4;
+            this.numPlot.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numPlot.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            // 
+            // numRezolu
+            // 
+            this.numRezolu.Location = new System.Drawing.Point(11, 144);
+            this.numRezolu.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.numRezolu.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numRezolu.Name = "numRezolu";
+            this.numRezolu.Size = new System.Drawing.Size(120, 20);
+            this.numRezolu.TabIndex = 3;
+            this.numRezolu.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numRezolu.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Разрешение";
+            // 
+            // BTStop
+            // 
+            this.BTStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BTStop.Location = new System.Drawing.Point(6, 64);
+            this.BTStop.Name = "BTStop";
+            this.BTStop.Size = new System.Drawing.Size(165, 45);
+            this.BTStop.TabIndex = 1;
+            this.BTStop.Text = "Stop";
+            this.BTStop.UseVisualStyleBackColor = true;
+            this.BTStop.Click += new System.EventHandler(this.BTStop_Click);
+            // 
+            // BTStart
+            // 
+            this.BTStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BTStart.Location = new System.Drawing.Point(6, 12);
+            this.BTStart.Name = "BTStart";
+            this.BTStart.Size = new System.Drawing.Size(165, 45);
+            this.BTStart.TabIndex = 0;
+            this.BTStart.Text = "Start";
+            this.BTStart.UseVisualStyleBackColor = true;
+            this.BTStart.Click += new System.EventHandler(this.BTStart_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(613, 446);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "GAME_LIFE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numPlot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numRezolu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BTStop;
+        private System.Windows.Forms.Button BTStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numPlot;
+        private System.Windows.Forms.NumericUpDown numRezolu;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ lify_game/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lify_game
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        private int resolution;
+        private GameEgein gameEgein;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void StartGame()
+        {
+            if (timer1.Enabled)
+                return;
+            numPlot.Enabled = false;
+            numRezolu.Enabled = false;
+            BTStart.Enabled = false;
+            resolution = (int)numRezolu.Value;
+            gameEgein = new GameEgein
+                (
+                rows: pictureBox1.Height / resolution,
+                cols:pictureBox1.Width/resolution,
+                density:(int)(numPlot.Minimum)+(int)(numPlot.Maximum)-(int)numPlot.Value
+                ) ;
+            Text = $"Generation{gameEgein.CurrentGeneration}";
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+            timer1.Start();
+
+        }
+        private void DrawNextGenretion()
+        
+        {
+            graphics.Clear(Color.Black);
+            var field = gameEgein.GetCurrentGeneration();
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    if (field[i, j])graphics.FillRectangle(Brushes.Crimson, i * resolution, j * resolution, resolution - 1, resolution - 1);
+                }
+            }
+            pictureBox1.Refresh();
+            Text = $"Generation{gameEgein.CurrentGeneration}";
+            gameEgein.NextGenration();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DrawNextGenretion();
+        }
+
+        private void BTStart_Click(object sender, EventArgs e)
+        {
+            StartGame();    
+        }
+
+        private void BTStop_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled)
+                return;
+            timer1.Stop();
+            numPlot.Enabled = true;
+            numRezolu.Enabled = true;
+            BTStart.Enabled = true;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!timer1.Enabled)
+                return;
+            if (e.Button == MouseButtons.Left)
+            {
+                var x = e.Location.X / resolution;
+                    var y=e.Location.Y/resolution; 
+                gameEgein.AddCell(x, y );
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                var x =e.Location.X / resolution;
+                var y =e.Location.Y/resolution;
+                gameEgein.RemoveCell(x, y );
+            }
+        }
+    }
+}
